c9f9d3d5c3d7a762908cc1287c38c040de345ba2
sadamali-oc <110493084+sadamali-oc@users.noreply.github.com>
2024-07-23 11:51:29 +0530

    update quality photo

--- main.js
+++ main.js
@@ -36,4 +36,43 @@
         menuIcon.classList.remove('bi-x');
     //     // menuIcon.classList.add('bi bi-list'); // Set initial icon state
         navbar.classList.remove('active');
+
+
+
+
     };
+
+
+
+    //scroll reveal
+
+    ScrollReveal({
+
+
+        distance:'80px',
+        duartion:2000,
+        delay:200,
+    
+
+     });
+
+
+
+     ScrollReveal().reveal('.home-content,heading', { origin:'top'});
+     ScrollReveal().reveal('.home-img, .services-container,.portfolio-box,.contact form', { origin: 'buttom' });
+     ScrollReveal().reveal('.home-contentt h1, .about-img', { origin: 'left'});
+     ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right' });
+
+     //typed js
+
+     
+
+const typed =new Typed('.multiple-text',{
+
+    strings:['Frontend Developer','Web Designer', 'IT Undergraduate','Tech Enthusiast'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+}
+)
